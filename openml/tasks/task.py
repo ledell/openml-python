a1a089519599c676e1616e0c941b58b05eff0c43
bilgecelik <bilgecelik@gmail.com>
2018-09-20 10:52:42 +0200

    Tasks WIP

--- openml/tasks/task.py
+++ openml/tasks/task.py
@@ -8,12 +8,24 @@ from .._api_calls import _read_url, _perform_api_call
 
 
 class OpenMLTask(object):
-    def __init__(self, task_id, task_type_id, task_type, data_set_id):
+    def __init__(self, task_id, task_type_id, task_type, data_set_id, data_splits_url, estimation_procedure_type,
+                 estimation_parameters, evaluation_measure, cost_matrix):
         self.task_id = int(task_id)
         self.task_type_id = int(task_type_id)
         self.task_type = task_type
         self.dataset_id = int(data_set_id)
+        self.estimation_procedure["data_splits_url"] = data_splits_url
         self.split = None
+        self.estimation_procedure = dict()
+        self.estimation_procedure["type"] = estimation_procedure_type
+        self.estimation_procedure["parameters"] = estimation_parameters
+        #
+        self.estimation_parameters = estimation_parameters
+        self.evaluation_measure = evaluation_measure
+        self.cost_matrix = cost_matrix
+
+        if cost_matrix is not None:
+            raise NotImplementedError("Costmatrix")
 
     def get_dataset(self):
         """Download dataset associated with task"""
@@ -22,85 +34,8 @@ class OpenMLTask(object):
     def download_split(self):
         """Download the OpenML split for a given task.
         """
-        cached_split_file = os.path.join(
-            _create_task_cache_dir(self.task_id), "datasplits.arff")
-
-        try:
-            split = OpenMLSplit._from_arff_file(cached_split_file)
-        # Add FileNotFoundError in python3 version (which should be a
-        # subclass of OSError.
-        except (OSError, IOError):
-            # Next, download and cache the associated split file
-            self._download_split(cached_split_file)
-            split = OpenMLSplit._from_arff_file(cached_split_file)
-
-        return split
-
-    def get_X_and_y(self):
-        """Get data associated with the current task.
-        
-        Returns
-        -------
-        tuple - X and y
-
-        """
-        dataset = self.get_dataset()
-        if self.task_type_id not in (1, 2, 3):
-            raise NotImplementedError(self.task_type)
-        X_and_y = dataset.get_data(target=self.target_name)
-        return X_and_y
-
-    def get_split_dimensions(self):
-        if self.split is None:
-            self.split = self.download_split()
-
-        return self.split.repeats, self.split.folds, self.split.samples
-
-
-
-class ClassificationTask(OpenMLTask):
-    def __init__(self, target_name, estimation_procedure_type, data_splits_url,
-                 estimation_parameters, evaluation_measure, cost_matrix,
-                 class_labels=None):
-        self.target_name = target_name
-        self.estimation_procedure = dict()
-        self.estimation_procedure["type"] = estimation_procedure_type
-        self.estimation_procedure["data_splits_url"] = data_splits_url
-        self.estimation_procedure["parameters"] = estimation_parameters
-        #
-        self.estimation_parameters = estimation_parameters
-        self.evaluation_measure = evaluation_measure
-        self.cost_matrix = cost_matrix
-        self.class_labels = class_labels
-
-        if cost_matrix is not None:
-            raise NotImplementedError("Costmatrix")
-
-    def get_train_test_split_indices(self, fold=0, repeat=0, sample=0):
-        # Replace with retrieve from cache
-        if self.split is None:
-            self.split = self.download_split()
-
-        train_indices, test_indices = self.split.get(repeat=repeat, fold=fold, sample=sample)
-        return train_indices, test_indices
-
-    def _download_split(self, cache_file):
-        try:
-            with io.open(cache_file, encoding='utf8'):
-                pass
-        except (OSError, IOError):
-            split_url = self.estimation_procedure["data_splits_url"]
-            split_arff = _read_url(split_url)
-
-            with io.open(cache_file, "w", encoding='utf8') as fh:
-                fh.write(split_arff)
-            del split_arff
-
-    def download_split(self):
-        """Download the OpenML split for a given task.
-        """
-
         if self.estimation_procedure["data_splits_url"]:
+
             cached_split_file = os.path.join(
                 _create_task_cache_dir(self.task_id), "datasplits.arff")
 
@@ -114,18 +49,39 @@ class ClassificationTask(OpenMLTask):
                 split = OpenMLSplit._from_arff_file(cached_split_file)
 
             return split
+
         else:
-            # not all tasks come with a split, e.g. in clustering the full dataset is always used
+            # Not all tasks come with a split, e.g. in clustering the full dataset is always used
             no_split = {0: {0: {0: (list(range(self.get_dataset().get_data().shape[0])),
                                     list(range(self.get_dataset().get_data().shape[0])))}}}
             split = OpenMLSplit('no_split', 'no actual split, all points in train and test', no_split)
             return split
 
-    def get_split_dimensions(self):
+    def get_X_and_y(self):
+        """Get data associated with the current task.
+
+        Returns
+        -------
+        tuple - X and y
+
+        """
+        dataset = self.get_dataset()
+        if self.task_type_id not in (1, 2, 3):
+            raise NotImplementedError(self.task_type)
+        X_and_y = dataset.get_data(target=self.target_name)
+        return X_and_y
+
+    def get_train_test_split_indices(self, fold=0, repeat=0, sample=0):
+        # Replace with retrieve from cache
         if self.split is None:
             self.split = self.download_split()
 
-        return self.split.repeats, self.split.folds, self.split.samples
+        train_indices, test_indices = self.split.get(repeat=repeat, fold=fold, sample=sample)
+        return train_indices, test_indices
+
+    def get_split_dimensions(self):
+        if self.split is None:
+            self.split = self.download_split()
 
     def push_tag(self, tag):
         """Annotates this task with a tag on the server.
@@ -149,13 +105,35 @@ class ClassificationTask(OpenMLTask):
         data = {'task_id': self.task_id, 'tag': tag}
         _perform_api_call("/task/untag", data=data)
 
+    def _create_task_cache_dir(task_id):
+        task_cache_dir = os.path.join(config.get_cache_directory(), "tasks", str(task_id))
 
-def _create_task_cache_dir(task_id):
-    task_cache_dir = os.path.join(config.get_cache_directory(), "tasks", str(task_id))
+        try:
+            os.makedirs(task_cache_dir)
+        except (IOError, OSError):
+            # TODO add debug information!
+            pass
+        return task_cache_dir
 
-    try:
-        os.makedirs(task_cache_dir)
-    except (IOError, OSError):
-        # TODO add debug information!
-        pass
-    return task_cache_dir
+class ClassificationTask(OpenMLTask):
+    def __init__(self, target_name, class_labels=None):
+        super().__init__(task_id, task_type_id, task_type, data_set_id, data_splits_url, estimation_procedure_type,
+                 estimation_parameters, evaluation_measure, cost_matrix)
+        self.target_name = target_name
+        self.class_labels = class_labels
+
+
+class ClusteringTask(OpenMLTask):
+    def __init__(self, number_of_targets):
+        super().__init__(task_id, task_type_id, task_type, data_set_id, data_splits_url, estimation_procedure_type,
+                         estimation_parameters, evaluation_measure, cost_matrix)
+        self.number_of_targets = number_of_targets
+
+
+
+
+
+
+
+
+
